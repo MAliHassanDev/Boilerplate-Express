e48bca15d2f92351da5815621a31b1afe4f79b84
ali <muhammadalihassanofficial@gmail.com>
2024-11-28 21:20:01 +0500

    refactor: remove routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,23 @@
-import express,{Express} from "express";
+import express, {Express} from "express";
 import compression from "compression";
-// Routes
-import homeRoutes from "@/routes/homeRoutes.js";
-import testRoutes from "@/routes/testRoutes.js"
+
 
 const app: Express = express();
-
 
 // don't identify express
 app.disable("x-powered-by");
 
 // parse req body of content application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 // parse req body of content json
 app.use(express.json());
-
 
 // compress res body for response
 app.use(compression());
 
 
 // assign routes
-app.use("/", homeRoutes);
-app.use("/test", testRoutes);
+
+
+export default app;
