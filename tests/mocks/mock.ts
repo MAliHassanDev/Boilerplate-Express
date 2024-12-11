78a47561ae70af25bccf4c77a8802ec13be2135f
ali <muhammadalihassanoffiial@gmail.com>
2024-12-11 20:28:04 +0500

    lint: remve unused var warning for mock file

--- tests/mocks/mock.ts
+++ tests/mocks/mock.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import mongoose, { Mongoose } from "mongoose";
 import { type Express } from "express";
